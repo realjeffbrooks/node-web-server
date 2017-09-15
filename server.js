13fb2d4533a5e90f50177f2fd89c2e4c5a0e2f54
triomedia <31011004+triomedia@users.noreply.github.com>
2017-09-15 17:15:34 -0600

    Added project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req,res) => {
   });
 });
 
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Our Projects',
+  });
+});
+
 app.get('*', (req,res) => {
   res.send({
     errorMessage: `Unable to handle request for page`,
